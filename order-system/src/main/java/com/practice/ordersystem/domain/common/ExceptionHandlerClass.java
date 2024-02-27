c7825bb3be5d9ec23efc8f3603dd1428299bbe35
SonJM <blisle@naver.com>
2024-02-27 17:42:51 +0900

    fix3

--- order-system/src/main/java/com/practice/ordersystem/domain/common/ExceptionHandlerClass.java
+++ order-system/src/main/java/com/practice/ordersystem/domain/common/ExceptionHandlerClass.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import javax.persistence.EntityNotFoundException;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
